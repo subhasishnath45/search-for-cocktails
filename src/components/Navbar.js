5ef53710ec2914ef65a55278726a6c1efbaa41f8
subhasishnath45 <subhasishnath45@gmail.com>
2021-06-07 22:56:43 +0530

    final commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../logo.svg'
+import logo from '../cocktailSearch-Logo.png'
 
 const Navbar = () => {
   return (
